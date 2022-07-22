894cc8e808beef768e91650629ce61c55918d96c
Douglas <douglaspereira04@hotmail.com>
2022-07-22 11:41:16 -0300

    remove unused files

--- src/sa/node.py
+++
@@ -1,9 +0,0 @@
-
-class Node:
-    def __init__(self, name, parent=None):
-        self.name = name
-        self.parent = parent
-        self.children = []
-
-        if parent:
-            self.parent.children.append(self)
